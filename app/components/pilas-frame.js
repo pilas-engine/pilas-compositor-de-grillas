7a64ffcc5e3c42da595bfde0a572fdb6d62964f3
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-09-22 23:44:22 -0300

    Haciendo la partición de la grilla funcione tanto con filas como columnas.

--- app/components/pilas-frame.js
+++ app/components/pilas-frame.js
@@ -78,7 +78,10 @@ export default Ember.Component.extend({
       this._draw_border(ctx, frameWidth, frameHeight);
     }
 
-    ctx.drawImage(image, currentFrame * frameWidth, 0, frameWidth, frameHeight, 0, 0, frameWidth, frameHeight);
+    let dx = Math.floor(currentFrame % rows);
+    let dy = Math.floor(currentFrame / rows);
+
+    ctx.drawImage(image, dx * frameWidth, dy * frameHeight, frameWidth, frameHeight, 0, 0, frameWidth, frameHeight);
 
     ctx.restore();
   },
--- app/controllers/editor/index.js
+++ app/controllers/editor/index.js
@@ -4,6 +4,9 @@ export default Ember.Controller.extend({
   queryParams: ['currentFrame', 'onionSkin', 'zoom'],
   currentFrame: 0,
   onionSkin: false,
+  totalFrames: Ember.computed('model.cols', 'model.rows', function() {
+    return (this.get('model.cols') * this.get('model.rows')) - 1;
+  }),
   zoom: 1,
   border: false,
   liveReload: Ember.inject.service(),
